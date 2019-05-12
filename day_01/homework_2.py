5d2943df9da32127ffe7acf647093a53cfd769e5
qimanchen <1033178199@qq.com>
2019-05-13 02:07:07 +0800

    add day01 file

---
+++ day_01/homework_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import turtle
+
+turtle.pensize(4)		# 绘制一个箭头，设置其大小
+turtle.pencolor('red')	# 设置其颜色
+turtle.forward(100)		# 向前移动100单位距离
+turtle.right(90)	# 箭头方向顺时针方向转动90度
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
---
+++ day_01/notes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+python查看版本号：
+1、命令行：python --version
+2、python交互环境
+	import sys
+	print(sys.version_info)
+	print(sys.version)
+
+注释：
+1、单行：#+空格
+2、多行：一对三引号
+
+print函数注意 -- sep参数表示多个元素直接的分割
+print('hello', 'world', sep=', ', end='!')
+# hello, world!
+"""
