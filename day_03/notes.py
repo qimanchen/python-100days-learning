f10bc75cc1cec8989a7ae0d098e857f004a305cd
qimanchen <1033178199@qq.com>
2019-05-20 17:11:31 +0800

    remove other filee

--- day_03/notes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-分支结构
-
-if
-尽量减少使用嵌套
-"""
--- day_08/notes.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-面向对象编程基础
-类中：通过单下划线的不能通过from import
-封装、继承、多态
-
-"""
